6b289078cfd27e4ba42aecf3a9130f7a2d6e2cf8
mertdemirbas <mertdemirbas@users.noreply.github.com>
2025-04-18 10:45:02 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-echo "📦 Ephemeris dosyaları indiriliyor..."
-
-# ephe klasörünü oluştur
-mkdir -p ephe
-
-# Dropbox zip dosyasını indir ve aç
-curl -L "https://www.dropbox.com/scl/fo/xxvijpxf6q1p6t1u039dy/AJgLs_s1Xl77xRgFCtLfn6I?rlkey=kftsbgcaguihzfxmj8db5xsqn&dl=1" -o ephe.zip
-
-# ZIP'i aç ve ephe klasörüne taşı
-unzip ephe.zip -d ephe-temp
-mv ephe-temp/* ephe/
-rm -rf ephe-temp ephe.zip
-
-echo "✅ Ephemeris verileri hazır, sunucu başlatılıyor..."
+echo "🚀 Sunucu başlatılıyor..."
 python app.py
